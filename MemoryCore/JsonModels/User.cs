5bdc139d7a25c96da1e3a3250e3643affc02c8a5
Genna Wingert <wingertbenjamin@yahoo.de>
2017-09-19 01:00:36 +0200

    Added client side user model

---
+++ MemoryCore/JsonModels/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemoryCore.JsonModels
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public FullName Name { get; set; }
+        public Address UserAddress { get; set; }
+        public long BirthDate { get; set; }
+        public bool EmailVerified { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Locale { get; set; }
+        public bool PhoneVerified { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ProfilePicture { get; set; }
+        public string DisplayName { get; set; }
+        public long UpdatedAt { get; set; }
+        public string ZoneInfo { get; set; }
+        public List<string> Roles { get; set; }
+
+        public struct FullName
+        {
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        public struct Address
+        {
+            public string Line1 { get; set; }
+            public string Line2 { get; set; }
+            public string Country { get; set; }
+            public string PostalCode { get; set; }
+            public string City { get; set; }
+            public string Locality { get; set; }
+        }
+    }
+}
