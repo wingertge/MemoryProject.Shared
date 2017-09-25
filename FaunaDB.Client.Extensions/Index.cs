091f717b09583eb38ab25f0ee335cd66bfaa0316
Genna Wingert <wingertbenjamin@yahoo.de>
2017-09-25 12:49:29 +0200

    a

--- FaunaDB.Client.Extensions/Index.cs
+++ FaunaDB.Client.Extensions/Index.cs
@@ -16,43 +16,28 @@ namespace FaunaDB.Extensions
     /// </summary>
     /// <typeparam name="T1">The type of the first composite parameter.</typeparam>
     /// <typeparam name="T2">The type of the second composite parameter.</typeparam>
-    public class CompositeIndex<T1, T2> : IEquatable<Tuple<T1, T2>>
+    public class CompositeIndex<T1, T2> : Tuple<T1, T2>
     {
-        public bool Equals(Tuple<T1, T2> other)
-        {
-            throw new NotImplementedException();
-        }
+        public CompositeIndex(T1 item1, T2 item2) : base(item1, item2) {}
     }
 
-    public class CompositeIndex<T1, T2, T3> : IEquatable<Tuple<T1, T2, T3>>
+    public class CompositeIndex<T1, T2, T3> : Tuple<T1, T2, T3>
     {
-        public bool Equals(Tuple<T1, T2, T3> other)
-        {
-            throw new NotImplementedException();
-        }
+        public CompositeIndex(T1 item1, T2 item2, T3 item3) : base(item1, item2, item3) { }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4> : IEquatable<Tuple<T1, T2, T3, T4>>
+    public class CompositeIndex<T1, T2, T3, T4> : Tuple<T1, T2, T3, T4>
     {
-        public bool Equals(Tuple<T1, T2, T3, T4> other)
-        {
-            throw new NotImplementedException();
-        }
+        public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4) : base(item1, item2, item3, item4) { }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4, T5> : IEquatable<Tuple<T1, T2, T3, T4, T5>>
+    public class CompositeIndex<T1, T2, T3, T4, T5> : Tuple<T1, T2, T3, T4, T5>
     {
-        public bool Equals(Tuple<T1, T2, T3, T4, T5> other)
-        {
-            throw new NotImplementedException();
-        }
+        public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5) : base(item1, item2, item3, item4, item5) { }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4, T5, T6> : IEquatable<Tuple<T1, T2, T3, T4, T5, T6>>
+    public class CompositeIndex<T1, T2, T3, T4, T5, T6> : Tuple<T1, T2, T3, T4, T5, T6>
     {
-        public bool Equals(Tuple<T1, T2, T3, T4, T5, T6> other)
-        {
-            throw new NotImplementedException();
-        }
+        public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6) : base(item1, item2, item3, item4, item5, item6) { }
     }
 }
