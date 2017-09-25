e7c5ffc3974373e7000ff143346ba87738dfc1dc
Genna Wingert <wingertbenjamin@yahoo.de>
2017-09-25 11:50:52 +0200

    a

---
+++ FaunaDB.Client.Extensions/SpecialTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FaunaDB.Query;
+
+namespace FaunaDB.Extensions
+{
+    public class Ref : IEquatable<string>
+    {
+        public string Value { get; set; }
+
+        public static implicit operator string(Ref @ref) => @ref.Value;
+        public static implicit operator Ref(string s) => new Ref(s);
+        public static implicit operator Expr(Ref @ref) => Language.Ref(@ref.Value);
+
+        public Ref() { }
+
+        public Ref(string s)
+        {
+            Value = s;
+        }
+
+        public bool Equals(string other)
+        {
+            return Value == other;
+        }
+    }
+}
