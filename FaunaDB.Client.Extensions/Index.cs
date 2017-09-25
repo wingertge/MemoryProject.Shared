8748e5e5fd5d2b338b5fa95a866bf1a33cad8fb3
Genna Wingert <wingertbenjamin@yahoo.de>
2017-09-25 13:36:51 +0200

    a

--- FaunaDB.Client.Extensions/Index.cs
+++ FaunaDB.Client.Extensions/Index.cs
@@ -16,28 +16,103 @@ namespace FaunaDB.Extensions
     /// </summary>
     /// <typeparam name="T1">The type of the first composite parameter.</typeparam>
     /// <typeparam name="T2">The type of the second composite parameter.</typeparam>
-    public class CompositeIndex<T1, T2> : Tuple<T1, T2>
+    public class CompositeIndex<T1, T2> : Tuple<T1, T2>, IEquatable<ValueTuple<T1, T2>>
     {
         public CompositeIndex(T1 item1, T2 item2) : base(item1, item2) {}
+
+        public bool Equals(ValueTuple<T1, T2> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(CompositeIndex<T1, T2> index, ValueTuple<T1, T2> other)
+        {
+            return index?.Equals(other) ?? false;
+        }
+
+        public static bool operator !=(CompositeIndex<T1, T2> index, (T1, T2) other)
+        {
+            return !(index == other);
+        }
     }
 
-    public class CompositeIndex<T1, T2, T3> : Tuple<T1, T2, T3>
+    public class CompositeIndex<T1, T2, T3> : Tuple<T1, T2, T3>, IEquatable<ValueTuple<T1, T2, T3>>
     {
         public CompositeIndex(T1 item1, T2 item2, T3 item3) : base(item1, item2, item3) { }
+
+        public bool Equals(ValueTuple<T1, T2, T3> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(CompositeIndex<T1, T2, T3> index, ValueTuple<T1, T2, T3> other)
+        {
+            return index?.Equals(other) ?? false;
+        }
+
+        public static bool operator !=(CompositeIndex<T1, T2, T3> index, ValueTuple<T1, T2, T3> other)
+        {
+            return !(index == other);
+        }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4> : Tuple<T1, T2, T3, T4>
+    public class CompositeIndex<T1, T2, T3, T4> : Tuple<T1, T2, T3, T4>, IEquatable<ValueTuple<T1, T2, T3, T4>>
     {
         public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4) : base(item1, item2, item3, item4) { }
+
+        public bool Equals(ValueTuple<T1, T2, T3, T4> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(CompositeIndex<T1, T2, T3, T4> index, ValueTuple<T1, T2, T3, T4> other)
+        {
+            return index?.Equals(other) ?? false;
+        }
+
+        public static bool operator !=(CompositeIndex<T1, T2, T3, T4> index, ValueTuple<T1, T2, T3, T4> other)
+        {
+            return !(index == other);
+        }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4, T5> : Tuple<T1, T2, T3, T4, T5>
+    public class CompositeIndex<T1, T2, T3, T4, T5> : Tuple<T1, T2, T3, T4, T5>, IEquatable<ValueTuple<T1, T2, T3, T4, T5>>
     {
         public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5) : base(item1, item2, item3, item4, item5) { }
+
+        public bool Equals(ValueTuple<T1, T2, T3, T4, T5> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(CompositeIndex<T1, T2, T3, T4, T5> index, ValueTuple<T1, T2, T3, T4, T5> other)
+        {
+            return index?.Equals(other) ?? false;
+        }
+
+        public static bool operator !=(CompositeIndex<T1, T2, T3, T4, T5> index, ValueTuple<T1, T2, T3, T4, T5> other)
+        {
+            return !(index == other);
+        }
     }
 
-    public class CompositeIndex<T1, T2, T3, T4, T5, T6> : Tuple<T1, T2, T3, T4, T5, T6>
+    public class CompositeIndex<T1, T2, T3, T4, T5, T6> : Tuple<T1, T2, T3, T4, T5, T6>, IEquatable<ValueTuple<T1, T2, T3, T4, T5, T6>>
     {
         public CompositeIndex(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5, T6 item6) : base(item1, item2, item3, item4, item5, item6) { }
+
+        public bool Equals(ValueTuple<T1, T2, T3, T4, T5, T6> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(CompositeIndex<T1, T2, T3, T4, T5, T6> index, ValueTuple<T1, T2, T3, T4, T5, T6> other)
+        {
+            return index?.Equals(other) ?? false;
+        }
+
+        public static bool operator !=(CompositeIndex<T1, T2, T3, T4, T5, T6> index, ValueTuple<T1, T2, T3, T4, T5, T6> other)
+        {
+            return !(index == other);
+        }
     }
 }
